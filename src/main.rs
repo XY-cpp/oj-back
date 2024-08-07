6ed05de6fdbfa1c1f31fbfbe1865f8f71994cb43
XY_cpp <xy_cpp@icloud.com>
2024-08-07 12:16:51 +0800

    [feat]添加主要配置

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use salvo::prelude::*;
+
+#[handler]
+async fn hello() -> String {
+  return String::from("Hello world");
+}
+
+#[tokio::main]
+async fn main() {
+  tracing_subscriber::fmt().init();
+
+  let router = Router::new().get(hello);
+  let acceptor = TcpListener::new("127.0.0.1:5800").bind().await;
+  Server::new(acceptor).serve(router).await;
 }
