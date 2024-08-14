c049eaf2c8cd97f52cf71c7b1855e7df5c4dcbc4
XY_cpp <xy_cpp@icloud.com>
2024-08-14 14:12:30 +0800

    [refactor]重构用户模块

--- src/module/user.rs
+++ src/module/user.rs
@@ -7,6 +7,8 @@ use serde_json::json;
 use crate::api::front::Res;
 use crate::utils::authority::{check_authority, Authority, Jwt};
 use crate::utils::db::db;
+use crate::utils::error::Error;
+use crate::{generate_error, handle_error};
 
 /// 对外路由接口
 pub fn init_router() -> Router {
@@ -33,212 +35,225 @@ crud!(User {});
 /// 用户注册
 ///
 /// # 前端请求地址
+///
 /// `/user/register`
 ///
 /// # 前端请求格式
-/// ```json
+///
+/// ```json5
 /// {
-///   accout: ...
-///   password: ...
+///   "accout": "...",
+///   "password": "..."
 /// }
 /// ```
 ///
 /// # 后端响应格式
-/// `success` 或 `error`
+///
+/// - 成功
+/// ```json5
+/// {
+///   "status": "success"
+/// }
+/// ```
+///
+/// - 失败
+/// ``` json5
+/// {
+///   "status": "error",
+///   "message": "data error", // 或 "internal error"
+///   "data": "..." // 出错数据
+/// }
+/// ```
 ///
 #[handler]
 async fn register(request: &mut Request, response: &mut Response) {
-  tracing::info!("Received a request to register a user.",);
-  match request.parse_json::<User>().await {
-    Ok(user) => {
-      let dbres = User::select_by_column(&db.clone(), "account", &user.account).await;
-      match dbres {
-        Ok(dbres) => {
-          if dbres.len() > 0 {
-            tracing::info!("Duplicate account found.");
-            response.render(Res::error("duplicate account"));
-          } else {
-            let mut user = user;
-            user.avatar = Some(String::from("http://127.0.0.1:8001/null"));
-            user.join_time = Some(DateTime::now());
-            user.auth = Some(Authority::User);
-            let dbinfo = User::insert(&db.clone(), &user).await;
-            match dbinfo {
-              Ok(dbinfo) => {
-                tracing::info!("{}", dbinfo);
-                response.render(Res::success());
-              }
-              Err(e) => {
-                tracing::error!("{:?}", e);
-                response.render(Res::error("database insertion failed"));
-              }
-            }
-          }
-        }
-        Err(e) => {
-          tracing::error!("{:?}", e);
-          response.render(Res::error("database query failed"));
-        }
-      }
+  async fn operation(request: &mut Request, response: &mut Response) -> Result<(), Error> {
+    tracing::info!("Received a request to register a user.",);
+    let user = request.parse_json::<User>().await?;
+    if user.account.is_none() || user.password.is_none() {
+      return generate_error!(
+        Error::WrongDataFormat,
+        "empty username or passowrd".to_string()
+      );
     }
-    Err(e) => {
-      tracing::error!("{:?}", e);
-      response.render(Res::error("json pharse failed"));
+    let dbres = User::select_by_column(&db.clone(), "account", &user.account).await?;
+    if dbres.len() > 0 {
+      return generate_error!(
+        Error::DuplicateData,
+        format!("account: {}", &user.account.unwrap()).to_string()
+      );
     }
+    let mut user = user;
+    user.avatar = Some(String::from("http://127.0.0.1:8001/null"));
+    user.join_time = Some(DateTime::now());
+    user.auth = Some(Authority::User);
+    let dbinfo = User::insert(&db.clone(), &user).await?;
+    tracing::info!("{}", dbinfo);
+    response.render(Res::success());
+    Ok(())
   }
+  handle_error!(operation(request, response), response);
 }
 
 /// 用户登录
 ///
 /// # 前端请求地址
+///
 /// `/user/login`
 ///
 /// # 前端请求格式
-/// ```json
+///
+/// ```json5
 /// {
-///   accout: ...
-///   password: ...
+///   "accout": "...",
+///   "password": "..."
 /// }
 /// ```
 ///
 /// # 后端响应格式
-/// `success` 或 `error`, 成功响应的`data`中包含`User`结构体的所有信息
+///
+/// - 成功
+/// ```json5
+/// {
+///   "status": "success",
+///   "data": [
+///     "...", //user的全部信息
+///   ]
+/// }
+/// ```
+///
+/// - 失败
+/// ``` json5
+/// {
+///   "status": "error",
+///   "message": "data error", // 或 "internal error"
+///   "data": "..." // 出错数据
+/// }
+/// ```
 ///
 #[handler]
 async fn login(request: &mut Request, response: &mut Response) {
-  tracing::info!("Received a request to login.",);
-  match request.parse_json::<User>().await {
-    Ok(user) => {
-      let dbres = User::select_by_column(&db.clone(), "account", &user.account).await;
-      match dbres {
-        Ok(dbres) => {
-          if dbres.len() == 0 {
-            tracing::info!("Account not found.");
-            response.render(Res::error("account not found"));
-          } else {
-            if user.password != dbres[0].password {
-              tracing::info!("Wrong password.");
-              response.render(Res::error("wrong password"));
-            } else {
-              let user = dbres[0].clone();
-              match Jwt::encode(user.id.unwrap(), user.auth.clone().unwrap()) {
-                Ok((token, exp)) => {
-                  tracing::info!(
-                    "User {} login successfully with token {}.",
-                    user.id.unwrap(),
-                    &token
-                  );
-                  let mut cookie = Cookie::new("token", token);
-                  cookie.set_expires(exp);
-                  response.add_cookie(cookie);
-                  response.render(Res::success_data(json!(&user)));
-                }
-                Err(e) => {
-                  tracing::error!("{:?}", e);
-                  response.render(Res::error("token generation failed"));
-                }
-              }
-            }
-          }
-        }
-        Err(e) => {
-          tracing::error!("{:?}", e);
-          response.render(Res::error("database query failed"));
-        }
-      }
+  async fn operation(request: &mut Request, response: &mut Response) -> Result<(), Error> {
+    tracing::info!("Received a request to login.",);
+    let user = request.parse_json::<User>().await?;
+    if user.account.is_none() || user.password.is_none() {
+      return generate_error!(
+        Error::WrongDataFormat,
+        "empty username or passowrd".to_string()
+      );
     }
-    Err(e) => {
-      tracing::error!("{:?}", e);
-      response.render(Res::error("json pharse failed"));
+    let dbres = User::select_by_column(&db.clone(), "account", &user.account).await?;
+    if dbres.len() == 0 {
+      return generate_error!(
+        Error::DataNotFound,
+        format!("account: {}", &user.account.unwrap()).to_string()
+      );
+    } else if user.password != dbres[0].password {
+      return generate_error!(
+        Error::WrongPassword,
+        format!("account: {}", &user.account.unwrap()).to_string()
+      );
     }
+    let user = dbres[0].clone();
+    let (token, exp) = Jwt::encode(user.id.unwrap(), user.auth.clone().unwrap())?;
+    tracing::info!(
+      "User {} login successfully with token {}.",
+      user.id.unwrap(),
+      &token
+    );
+    let mut cookie = Cookie::new("token", token);
+    cookie.set_expires(exp);
+    response.add_cookie(cookie);
+    response.render(Res::success_data(json!(&user)));
+    Ok(())
   }
+  handle_error!(operation(request, response), response);
 }
 
 /// 用户修改
 ///
 /// # 前端请求地址
-/// `/user/update`
+///
+/// `/user/login`
 ///
 /// # 前端请求格式
-/// ```json
+///
+/// ```json5
 /// {
-///   id: ... //要修改的用户id
-///   ......  //要修改的用户数据
+///   "id": [num], // 要修改的用户编号
+///   "...", //需要修改的数据
 /// }
 /// ```
 ///
 /// # 后端响应格式
-/// `success` 或 `error`
+///
+/// - 成功
+/// ```json5
+/// {
+///   "status": "success",
+/// }
+/// ```
+///
+/// - 失败
+/// ``` json5
+/// {
+///   "status": "error",
+///   "message": "data error", // 或 "internal error"
+///   "data": "..." // 出错数据
+/// }
+/// ```
 ///
 #[handler]
 async fn update(request: &mut Request, response: &mut Response) {
-  tracing::info!("Received a request to update a user.",);
-  if let None = request.cookie("token") {
-    tracing::info!("User not login.");
-    response.render(Res::error("user not login"));
-    return;
-  }
-  match request.parse_json::<User>().await {
-    Ok(user) => {
-      let dbres = User::select_by_column(&db.clone(), "id", &user.id).await;
-      match dbres {
-        Ok(dbres) => {
-          if dbres.len() == 0 {
-            tracing::info!("User not found.");
-            response.render(Res::error("user not found"));
-          } else {
-            match check_authority(
-              request.cookie("token").unwrap().value().to_string(),
-              user.id.unwrap(),
-              Authority::Admin,
-            ) {
-              false => {
-                tracing::info!("User {} has no authority.", user.id.unwrap());
-                response.render(Res::error("no authority"));
-              }
-              true => {
-                let mut new_user = dbres[0].clone();
-                if let Some(avatar) = user.avatar {
-                  new_user.avatar = Some(avatar);
-                }
-                if let Some(account) = user.account {
-                  new_user.account = Some(account);
-                }
-                if let Some(password) = user.password {
-                  new_user.password = Some(password);
-                }
-                if let Some(join_time) = user.join_time {
-                  new_user.join_time = Some(join_time);
-                }
-                if let Some(authority) = user.auth {
-                  new_user.auth = Some(authority);
-                }
-                let dbinfo = User::update_by_column(&db.clone(), &new_user, "id").await;
-                match dbinfo {
-                  Ok(dbinfo) => {
-                    tracing::info!("{}", dbinfo);
-                    response.render(Res::success());
-                  }
-                  Err(e) => {
-                    tracing::error!("{:?}", e);
-                    response.render(Res::error("database insertion failed"));
-                  }
-                }
-              }
-            }
-          }
+  async fn operation(request: &mut Request, response: &mut Response) -> Result<(), Error> {
+    tracing::info!("Received a request to update a user.",);
+    if let None = request.cookie("token") {
+      return generate_error!(Error::NoAuthority, "user not login".to_string());
+    }
+    let user = request.parse_json::<User>().await?;
+    if user.id.is_none() {
+      return generate_error!(Error::WrongDataFormat, "".to_string());
+    }
+    let dbres = User::select_by_column(&db.clone(), "id", &user.id).await?;
+    if dbres.len() == 0 {
+      return generate_error!(Error::DataNotFound, user.id.unwrap().to_string());
+    }
+    match check_authority(
+      request.cookie("token").unwrap().value().to_string(),
+      user.id.unwrap(),
+      Authority::Admin,
+    ) {
+      false => {
+        return generate_error!(
+          Error::NoAuthority,
+          format!("user {} has no authority", user.id.unwrap()).to_string()
+        );
+      }
+      true => {
+        let mut new_user = dbres[0].clone();
+        if let Some(avatar) = user.avatar {
+          new_user.avatar = Some(avatar);
         }
-        Err(e) => {
-          tracing::error!("{:?}", e);
-          response.render(Res::error("database query failed"));
+        if let Some(account) = user.account {
+          new_user.account = Some(account);
         }
+        if let Some(password) = user.password {
+          new_user.password = Some(password);
+        }
+        if let Some(join_time) = user.join_time {
+          new_user.join_time = Some(join_time);
+        }
+        if let Some(authority) = user.auth {
+          new_user.auth = Some(authority);
+        }
+        let dbinfo = User::update_by_column(&db.clone(), &new_user, "id").await?;
+        tracing::info!("{}", dbinfo);
+        response.render(Res::success());
+        Ok(())
       }
     }
-    Err(e) => {
-      tracing::error!("{:?}", e);
-      response.render(Res::error("json pharse failed"));
-    }
   }
+  handle_error!(operation(request, response), response);
 }
 
 /// 用户查询
@@ -247,42 +262,54 @@ async fn update(request: &mut Request, response: &mut Response) {
 /// `/user/query`
 ///
 /// # 前端请求格式
-/// ```json
+/// ```json5
 /// {
-///   id: ... //要查询的用户id
+///   "id": [num], //要查询的用户id
 /// }
 /// ```
 ///
 /// # 后端响应格式
-/// `success` 或 `error`, 成功响应的`data`中包含`User`结构体的所有信息
+///
+/// - 成功
+/// ```json5
+/// {
+///   "status": "success",
+///   "data": [
+///     "...", //user的全部信息
+///   ]
+/// }
+/// ```
+///
+/// - 失败
+/// ``` json5
+/// {
+///   "status": "error",
+///   "message": "data error", // 或 "internal error"
+///   "data": "..." // 出错数据
+/// }
+/// ```
 ///
 #[handler]
 async fn query(request: &mut Request, response: &mut Response) {
-  tracing::info!("Received a request to query.",);
-  match request.parse_json::<User>().await {
-    Ok(user) => {
-      let dbres = User::select_by_column(&db.clone(), "id", &user.id).await;
-      match dbres {
-        Ok(dbres) => {
-          if dbres.len() == 0 {
-            tracing::info!("User not found.");
-            response.render(Res::error("user not found"));
-          } else {
-            tracing::info!("Query successfully.");
-            response.render(Res::success_data(json!(&dbres[0])));
-          }
-        }
-        Err(e) => {
-          tracing::error!("{:?}", e);
-          response.render(Res::error("database query failed"));
-        }
-      }
+  async fn operation(request: &mut Request, response: &mut Response) -> Result<(), Error> {
+    tracing::info!("Received a request to query.",);
+    let user = request.parse_json::<User>().await?;
+    if user.id.is_none() {
+      return generate_error!(Error::WrongDataFormat, "".to_string());
     }
-    Err(e) => {
-      tracing::error!("{:?}", e);
-      response.render(Res::error("json pharse failed"));
+    let dbres = User::select_by_column(&db.clone(), "id", &user.id).await?;
+    if dbres.len() == 0 {
+      return generate_error!(
+        Error::DataNotFound,
+        format!("account: {}", &user.account.unwrap()).to_string()
+      );
+    } else {
+      tracing::info!("Query user {} successfully.", &dbres[0].id.unwrap());
+      response.render(Res::success_data(json!(&dbres[0])));
     }
+    Ok(())
   }
+  handle_error!(operation(request, response), response);
 }
 
 /// 用户删除
@@ -293,46 +320,56 @@ async fn query(request: &mut Request, response: &mut Response) {
 /// # 前端请求格式
 /// ```json
 /// {
-///   id: ... //要查询的用户id
+///   id: ... //要删除的用户id
 /// }
 /// ```
 ///
 /// # 后端响应格式
-/// `success` 或 `error`
 ///
+/// - 成功
+/// ```json5
+/// {
+///   "status": "success",
+/// }
+/// ```
+///
+/// - 失败
+/// ``` json5
+/// {
+///   "status": "error",
+///   "message": "data error", // 或 "internal error"
+///   "data": "..." // 出错数据
+/// }
+/// ```
 #[handler]
 async fn delete(request: &mut Request, response: &mut Response) {
-  if let None = request.cookie("token") {
-    tracing::info!("User not login.");
-    response.render(Res::error("user not login"));
-    return;
-  }
-  match request.parse_json::<User>().await {
-    Ok(user) => {
-      let dbinfo = User::delete_by_column(&db.clone(), "id", &user.id).await;
-      match dbinfo {
-        Ok(_) => {
-          if !check_authority(
-            request.cookie("token").unwrap().value().to_string(),
-            user.id.unwrap(),
-            Authority::Admin,
-          ) {
-            tracing::info!("User {} has no authority.", user.id.unwrap());
-            response.render(Res::error("no authority"));
-          } else {
-            tracing::info!("Delete user {} successfully", &user.id.unwrap());
-            response.render(Res::success());
-          }
-        }
-        Err(e) => {
-          tracing::error!("{:?}", e);
-          response.render(Res::error("database query failed"));
-        }
+  async fn operation(request: &mut Request, response: &mut Response) -> Result<(), Error> {
+    tracing::info!("Received a request to delete a user.",);
+    if let None = request.cookie("token") {
+      return generate_error!(Error::NoAuthority, "user not login".to_string());
+    }
+    let user = request.parse_json::<User>().await?;
+    if user.id.is_none() {
+      return generate_error!(Error::WrongDataFormat, "".to_string());
+    }
+    let _ = User::delete_by_column(&db.clone(), "id", &user.id).await?;
+    match check_authority(
+      request.cookie("token").unwrap().value().to_string(),
+      user.id.unwrap(),
+      Authority::Admin,
+    ) {
+      false => {
+        return generate_error!(
+          Error::NoAuthority,
+          format!("user {} has no authority", user.id.unwrap()).to_string()
+        );
+      }
+      true => {
+        tracing::info!("Delete user {} successfully", &user.id.unwrap());
+        response.render(Res::success());
+        Ok(())
       }
     }
-    Err(e) => {
-      tracing::error!("{:?}", e);
-      response.render(Res::error("json pharse failed"));
-    }
   }
+  handle_error!(operation(request, response), response);
 }
---
+++ src/utils/error.rs
@@ -0,0 +1,67 @@
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+  #[error("Json parse error: {0}")]
+  JsonParseError(#[from] salvo::http::ParseError),
+
+  #[error("Rbatis error: {0}")]
+  RbatisError(#[from] rbatis::rbdc::Error),
+
+  #[error("Token error error: {0}")]
+  TokenError(#[from] jsonwebtoken::errors::Error),
+
+  #[error("Duplicate data: {0}")]
+  DuplicateData(String),
+
+  #[error("Wrong data format: {0}")]
+  WrongDataFormat(String),
+
+  #[error("Wrong password: {0}")]
+  WrongPassword(String),
+
+  #[error("Data not found: {0}")]
+  DataNotFound(String),
+
+  #[error("No authority: {0}")]
+  NoAuthority(String),
+}
+
+/// 错误生成宏
+#[macro_export]
+macro_rules! generate_error {
+  ($variant:path, $dict:expr) => {
+    Err($variant($dict))
+  };
+}
+
+/// 错误处理宏
+#[macro_export]
+macro_rules! handle_error {
+  ($operation:expr, $response:expr) => {
+    if let Err(e) = $operation.await {
+      match e {
+        self::Error::JsonParseError(_)
+        | self::Error::RbatisError(_)
+        | self::Error::TokenError(_) => {
+          tracing::error!("{}", e);
+          $response.render(
+            Res::new()
+              .status("error")
+              .message("internal error")
+              .data(json!(e.to_string()))
+              .to_json(),
+          );
+        }
+        _ => {
+          tracing::warn!("{}", e);
+          $response.render(
+            crate::api::front::Res::new()
+              .status("error")
+              .message("data error")
+              .data(json!(e.to_string()))
+              .to_json(),
+          );
+        }
+      }
+    }
+  };
+}
